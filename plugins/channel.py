ba6bae0a299b6fd2b395146e5692afbba25b9715
PRIME BOT'z <sahidhasannayem@gmail.com>
2024-10-11 16:22:11 +0600

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -10,7 +10,6 @@ from info import CHANNELS, MOVIE_UPDATE_CHANNEL, ADMINS , LOG_CHANNEL
 from database.ia_filterdb import save_file, unpack_new_file_id
 from utils import get_poster, temp
 import re
-import os
 from database.users_chats_db import db
 
 processed_movies = set()
@@ -96,7 +95,7 @@ async def send_movie_updates(bot, file_name, caption, file_id):
         
         btn = [
             [InlineKeyboardButton('🎥 𝗚𝗲𝘁 𝗙𝗶𝗹𝗲 📂', url=f'https://t.me/{temp.U_NAME}?start=pm_mode_file_{ADMINS[0]}_{file_id}')],
-            [InlineKeyboardButton('🔰 𝗠𝗼𝘃𝗶𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗚𝗿𝗼𝘂𝗽 🔰', url=GRP_LNK)]
+            [InlineKeyboardButton('🔰 𝗠𝗼𝘃𝗶𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 𝗚𝗿𝗼𝘂𝗽 🔰', url='https://t.me/+OG3sftDEbZ9kMzFl')]
         ]
         reply_markup = InlineKeyboardMarkup(btn)
         
