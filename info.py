6b4485841f05f7a9d54c20c0a3672ad270840f62
PRIME BOT'z <sahidhasannayem@gmail.com>
2024-10-12 17:15:56 +0600

    Update info.py

--- info.py
+++ info.py
@@ -56,7 +56,7 @@ TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "86400"))
 THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "86400"))
 #---------------------------------------------------------------
 #---------------------------------------------------------------
-LANGUAGES = ["hindi", "english", "banga", "tamil", "kannada", "malayalam", "bengali", "Telugu", "gujarati", "punjabi"]
+LANGUAGES = ["hindi", "english", "bangla", "tamil", "kannada", "malayalam", "bengali", "Telugu", "gujarati", "punjabi"]
 QUALITIES = ["HdRip","web-dl" ,"bluray", "hdr", "fhd" , "240p", "360p", "480p", "540p", "720p", "960p", "1080p", "1440p", "2K", "2160p", "4k", "5K", "8K"]
 YEARS = [f'{i}' for i in range(2024 , 2002,-1 )]
 SEASONS = [f'season {i}'for i in range (1 , 23)]
